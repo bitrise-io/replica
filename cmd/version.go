9ec94c8197c6f150914013a8e9bf36e371165b82
Viktor Benei <viktor.benei@gmail.com>
2016-11-15 14:23:10 +0100

    version command

---
+++ cmd/version.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/bitrise-tools/replica/version"
+	"github.com/spf13/cobra"
+)
+
+var (
+	isFullVersionPrint = false
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints version",
+	Long:  `Prints version`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(version.VERSION)
+
+		if isFullVersionPrint {
+			fmt.Println()
+			fmt.Println("go: " + runtime.Version())
+			fmt.Println("arch: " + runtime.GOARCH)
+			fmt.Println("os: " + runtime.GOOS)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+	versionCmd.Flags().BoolVar(&isFullVersionPrint, "full", false, "Full version")
+}
